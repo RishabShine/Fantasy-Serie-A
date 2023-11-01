879665d4597d326aeedc6a1c5adec86a0c3f251f
Om Patel <ompatel801@icloud.com>
2023-11-01 15:44:25 -0400

    updated app.py file

--- app.py
+++ app.py
@@ -93,3 +93,115 @@ def register():
 
     else:
         return render_template("register.html")
+
+     
+# functions
+
+# points system
+
+# home screen
+
+@app.route("/", methods=["GET"])
+def index():
+    
+    players = db.execute("SELECT players.name, players.position, player.value, team.crest, team.color FROM players JOIN teams ON teams.id = players.team_id WHERE player.id IN (SELECT player_id FROM ownership WHERE user_id = ? AND league_id = NULL", session["user_id"])
+
+    return render_template("index")
+
+# league home screen
+
+# ! condition for leaving league (if league_id != NULL cannot join other league, method of removing data when leaving league)
+
+@app.route("/league", methods=["GET"])
+def league():
+    
+    cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]
+    players = db.execute("SELECT players.name, players.position, player.value, team.crest, team.color FROM players JOIN teams ON teams.id = players.team_id WHERE player.id IN (SELECT player_id FROM ownership WHERE user_id = ? AND league_id IN (SELECT id FROM league WHERE id IN (SELECT league_id FROM users WHERE id = ?))", session["user_id"], session["user_id"])
+
+# create league
+
+@app.route("/createleague", methods=["GET", "POST"])
+def create_league():
+    if request.method == "POST":
+        return render_template("league.html")
+    else:
+        return render_template("createleague.html")
+
+
+# drafting
+
+# trading (sending offers, and accepting trades)
+
+# sending offer 
+# ! *** (how to set up league_id??)
+
+@app.route("/offer", methods=["GET", "POST"])
+def offer():
+    if request.method == "POST":
+
+        # ! add offer to offers table
+
+        return redirect("/")
+
+    else:
+        
+        # getting league id from user
+
+        league_id = db.execute("SELECT league_id FROM users WHERE id = ?", session["user_id"])
+
+        # checking what players the user is able to trade (not currently in an offer)
+
+        available_players = db.execute("SELECT player_id, name FROM players WHERE player_id IN (SELECT player_id FROM ownership WHERE user_id = ? AND league_id = ? AND player_id NOT IN offers WHERE league_id IN (SELECT player_s FROM offers WHERE sender = ? AND league_id ))", session["user_id"], league_id, session["user_id"], league_id)
+        
+        # checking how much extra space the user has in their team
+
+        total_players = db.execute("SELECT player_id FROM players WHERE player_id IN (SELECT player_id FROM ownership WHERE user_id = ? AND league_id = ?)", session["user_id"], league_id)
+
+        space = 14 - (len(total_players))
+
+        # displaying players who are owned in the league
+
+        owned = db.execute("SELECT players.player_id, players.name, users.username FROM ownership JOIN users ON ownership.user_id = users.id WHERE users.league_id = ? AND users.id NOT IN (SELECT id FROM users WHERE id = ?)", league_id, session["user_id"])
+
+        # displaying players that are not owned in the laegue
+
+        free_agent = ("SELECT player_id, name FROM players WHERE player_id NOT IN (SELECT players.player_id, players.name, users.username FROM ownership JOIN users ON ownership.user_id = users.id WHERE users.league_id = ? AND users.id NOT IN (SELECT id FROM users WHERE id = ?)))", league_id, session["user_id"])
+
+        # ! ensure that when making an offer there are not extra players, and a balance of positions (one trade at a time?)
+        return render_template("offer.html", space=space, available_players=available_players, owned=owned, free_agent=free_agent)
+
+# accepted trades
+
+@app.route("/trade", methods=["GET", "POST"])
+def trade():    
+    if request.method == "POST":
+
+        # gettting information about accepted offer
+        # ! create a condition so that if a user accepts a trade for a player which someone else sent an offer for, the offer is removed (check should be run after any accepted trade)
+        # ! how to connect based on league
+
+        offer = db.execute("SELECT sender, receiver, player_s, player_r FROM offers WHERE reciever = ? OR sender = ")
+
+        # sending players to new owners
+        
+        if not offer["player_s"] == "NULL":
+            db.execute("INSERT INTO ownership (player_id, user_id, league_id) VALUES (?, ?, ?)", offer["player_s"], offer["receiver"], offer["league_id"])
+        if not offer["player_r"] == "NULL":
+            db.execute("INSERT INTO ownership (player_id, user_id, league_id) VALUES (?, ?, ?)", offer["player_r"], offer["sender"], offer["league_id"])
+
+        # removing traded players from previous owners ownership and removing the offer
+
+        db.execute("DELETE FROM ownership WHERE user_id = ? AND player_id = ? AND league_id = ?", offer["sender"], offer["player_s"], offer["league_id"])
+        db.execute("DELETE FROM ownership WHERE user_id = ? AND player_id = ? AND league_id = ?", offer["receiver"], offer["player_r"], offer["league_id"])
+        db.execute("DELETE FROM offers WHERE sender = ? AND receiver = ? AND player_s = ? AND player_r = ? AND league_id = ?", offer["sender"], offer["receiver"], offer["player_s"], offer["player_r"], offer["league_id"])
+
+        return redirect("/league")
+
+    else:
+       
+        # viewing trade offers
+
+        offers_received = db.execute("SELECT * FROM offers WHERE receiver = ?", session["user_id"])
+        offers_sent = db.execute("SELECT * FROM offers WHERE sender = ?", session["user_id"])
+
+        return render_template("trades.html", offers_receiver=offers_received, offers_sent=offers_sent)
